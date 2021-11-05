5a121ec21e53534989e637db2908fd34d1a4a798
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-04 21:58:03 -0300

    inserindo typagem de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { v4 as uuid } from "uuid";
 
 import { Category } from "../model/Category";
 
@@ -10,13 +9,13 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = {
-    id: uuid(),
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
     created_at: new Date(),
-  };
-
+  });
   categories.push(category);
 
   return response.status(201).json(category);
