0a804f2dc05e9ad244fafa80392c74907cc11d5c
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-06 16:16:17 -0300

    createCategoryController

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+class CreateCategoryController {
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+  handle(request: Request, response: Response): Response {
+    const { name, description } = request.body;
+
+    try {
+      this.createCategoryUseCase.execute({ name, description });
+
+      return response.status(201).send();
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+  }
+}
+
+export { CreateCategoryController };
