79de600e0523c66bc4ca8f393fdeb1d3929c2873
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-10 21:27:45 -0300

    created migrationc categories

---
+++ src/database/migrations/1636589713236-CreateCategories.ts
@@ -0,0 +1,36 @@
+import { query } from 'express';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCategories1636589713236 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created-at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
