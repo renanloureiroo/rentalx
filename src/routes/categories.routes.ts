cae0360d7da15b909081ba2ae737c8d34542d04a
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-12 00:17:49 -0300

    refactored importCategory useCase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
-import { importCategoryController } from '../modules/cars/useCases/importCategory';
+import { ImportCategoryController } from '../modules/cars/useCases/importCategory/importCategoryController';
 import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListCategoriesControlles';
 
 const categoriesRoutes = Router();
@@ -12,13 +12,15 @@ const upload = multer({
 
 const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
-
+const importCategoryController = new ImportCategoryController();
 categoriesRoutes.post('/', createCategoryController.handle);
 
 categoriesRoutes.get('/', listCategoriesController.handle);
 
-categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoryController.handle(request, response);
-});
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  importCategoryController.handle
+);
 
 export { categoriesRoutes };
