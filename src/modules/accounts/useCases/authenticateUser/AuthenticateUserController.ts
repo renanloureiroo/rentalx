8af835b3b5ff543b397428a8dcc1c820e96b8423
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-13 11:29:34 -0300

    refactored response of AuthenticateUserCOntroller

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -9,12 +9,12 @@ class AuthenticateUserController {
     const authenticateUserUseCase = container.resolve(AuthenticateUseUseCase);
 
     try {
-      const authenticated = await authenticateUserUseCase.execute({
+      const token = await authenticateUserUseCase.execute({
         email,
         password,
       });
 
-      return response.json(authenticated);
+      return response.json(token);
     } catch (err) {
       return response.status(401).json({
         error: err.message,
