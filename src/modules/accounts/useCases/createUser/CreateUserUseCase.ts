7535434bebd096574c43e53d64add63f72fa659c
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-12 21:57:27 -0300

    CreateUser useCase

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,17 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute(data: ICreateUserDTO): Promise<void> {
+    await this.userRepository.create(data);
+  }
+}
+export { CreateUserUseCase };
