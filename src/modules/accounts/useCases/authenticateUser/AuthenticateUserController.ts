12d87aa7dcd8be145fcfca433f9fa17b11a1fe0b
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-13 11:37:42 -0300

    refactored status code err AuthenticateUserController

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -16,7 +16,7 @@ class AuthenticateUserController {
 
       return response.json(token);
     } catch (err) {
-      return response.status(401).json({
+      return response.status(400).json({
         error: err.message,
       });
     }
