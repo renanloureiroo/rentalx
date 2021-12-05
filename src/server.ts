14b20c2553cfeaf4028eaefd7f9054cb0b46e6e0
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-12-05 01:04:10 -0300

    recreate container

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ import './database';
 
 import './shared/container/index';
 
-import { AppError } from './errors/AppError';
+import { AppError } from '@errors/AppError';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -18,20 +19,17 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        error: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error ${err.message}`,
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      error: err.message,
     });
-    return next();
   }
-);
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log('Server is running!'));
